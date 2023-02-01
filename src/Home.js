5573505835b5b99bd1e20a9424843d1c58c31b1d
yeyel <arielglvz@gmail.com>
2023-02-02 01:18:58 +0800

    feat: (main) handling fetch errors

--- src/Home.js
+++ src/Home.js
@@ -3,21 +3,37 @@ import BlogList from "./Bloglist";
 
 const Home = () => {
     const [blogs, setBlogs] = useState(null);
- 
+    const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
+
     useEffect(() => {
-        fetch('http://localhost:8000/blogs')
-            .then(res => {
-                return res.json();
-            })
-            .then(data => {
-                console.log(data);
-                setBlogs(data);
-            })
+        setTimeout(() => {
+            fetch('http://localhost:8000/blogs')
+                .then(res => {
+                    console.log(res);
+                    if(!res.ok) {
+                        throw Error('Could not fetch the data for the resource');
+                    }
+
+                    return res.json();
+                })
+                .then(data => {
+                    setBlogs(data);
+                    setIsPending(false);
+                    setError(null);
+                })
+                .catch(err => {
+                    setError(err.message);
+                    setIsPending(false);
+                })
+        }, 1000);
     }, []);
 
     return (  
         <div className="home">
-           { blogs && <BlogList blogs={ blogs }  title = { "All Blogs!" } /> }
+            { error && <div>{ error }</div>}
+            { isPending && <div>Loading...</div> }
+            { blogs && <BlogList blogs={ blogs }  title = { "All Blogs!" } /> }
         </div>  
         
     );
