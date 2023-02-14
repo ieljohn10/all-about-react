9f878e80231cd74e7d98bacd09f12fd6cb2eede0
yeyel <arielglvz@gmail.com>
2023-02-15 07:11:30 +0800

    feat: (all-about-react) filter

--- src/newLearning/filterFunction.js
+++ src/newLearning/filterFunction.js
@@ -1,3 +1,5 @@
+// To filter through an array inside of an object array in JavaScript, you can use the filter() method in combination with the some() method. Here is an example:
+
 const objectArray = [
     { id: 1, name: "Alice", hobbies: ["reading", "painting", "swimming"] },
     { id: 2, name: "Bob", hobbies: ["hiking", "running"] },
@@ -7,4 +9,18 @@ const objectArray = [
   const filteredArray = objectArray.filter(obj => obj.hobbies.some(hobby => hobby === "reading"));
   
   console.log(filteredArray);
+
+ 
+//    In this example, objectArray is an array of objects that each contain an id, a name, and a hobbies array. 
+//    To filter through this array and only keep the objects that have "reading" as one of their hobbies, we use the filter() method and pass in a callback function. 
+//    Inside this function, we use the some() method to check if the hobbies array contains the string "reading". 
+//    The some() method returns a boolean value that indicates whether at least one element in the array satisfies the condition. 
+//    If some() returns true, the object is kept in the filtered array.
+
+//    The resulting filteredArray contains only the objects that have "reading" as one of their hobbies.
+
+
+
+
+
   
